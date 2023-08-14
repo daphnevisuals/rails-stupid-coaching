c92c61b4109b21a9a36e0191060f380d64a72ca5
Daphne Staruch <daphstr@gmail.com>
2023-08-14 12:29:46 -0400

    added the answer to ask form + first conditional

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,12 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
+  def answer
+    @question = params[:question]
+    @answer = ''
+    if @question = 'I am going to work'
+      @answer = 'Great'
+    end
+  end
 end
