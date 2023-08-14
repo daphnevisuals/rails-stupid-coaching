491ddf841002fa3d246c78777b9613ac66c74ca4
Daphne Staruch <daphstr@gmail.com>
2023-08-14 13:51:10 -0400

    first conditional fix and second on answer action of questions control

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,8 +5,10 @@ class QuestionsController < ApplicationController
   def answer
     @question = params[:question]
     @answer = ''
-    if @question = 'I am going to work'
+    if @question == 'I am going to work'
       @answer = 'Great'
+    elsif @question.end_with?("?")
+      @answer = 'Silly question, get dressed and go to work!'
     end
   end
 end
