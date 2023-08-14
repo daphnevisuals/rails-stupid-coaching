95060d73ad4441d14e589e1a8811faaf7c444d68
Daphne Staruch <daphstr@gmail.com>
2023-08-14 13:53:27 -0400

    if conditional done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,8 +7,10 @@ class QuestionsController < ApplicationController
     @answer = ''
     if @question == 'I am going to work'
       @answer = 'Great'
-    elsif @question.end_with?("?")
+    elsif @question.end_with?('?')
       @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
